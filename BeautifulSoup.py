c30d24aadef35e11c09389c23352d954893e61fd
DaniiVCGamer <daniel.vargas@correounivalle.edu.co>
2021-09-14 23:14:05 -0500

    Se añadió la deteccion del lenguaje del texto, expansión de contracciones y lematizacion de textos en ingles

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,12 +1,13 @@
 # Import libraries
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import ssl
 import os
 import errno
-from nltk.corpus import stopwords
-# import nltk    # Descomentar esto si es la primera vez que ejecuta el código
 import re
+import nltk
 
 # Crear un certificado SSL por defecto, esto por si algunas páginas no lo tienen:
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -17,13 +18,25 @@ path = "Transcripciones/"
 # Ruta en donde se encuentra el archivo .txt donde se almacena la lista de las url de cada página web
 enlaces = "Paginas de becas.txt"
 
+# Definir los diferentes idiomas que aparecerán en las páginas:
+languages = ["spanish", "english"]
+
+'''
+Descomentar estas lineas de código si es la primera vez que ejecuta el código
+'''
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+
 # Crear una lista de las palabras "Stopwords" que se eliminarán del texto
-# nltk.download('stopwords')  # Descomentar esto si es la primera vez que ejecuta el código
 stop_words_sp = set(stopwords.words('spanish'))
 stop_words_en = set(stopwords.words('english'))
 
 # Concatenar las stopwords
 stop_words = stop_words_sp | stop_words_en
+
+# Definir un lematizador para el idioma inglés
+lemmatizer = WordNetLemmatizer()
 
 
 def getwebsite(url):
@@ -35,10 +48,11 @@ def getwebsite(url):
 
 def escribir(filename, text):
     file = open(filename, 'w', encoding="utf-8")  # Abrir en modo "w" Escritura
-    text = text.lower()
-    text = eliminar_stopwords(text)
-    text = EliminarSimbolos(text)
-    text = re.sub("\n+", " ", text)
+
+    # A continuación se hace el llamado a las funciones que realizan la limpieza del texto
+    text = limpieza_textos(text)
+
+    # Una vez hecha la limpieza, se escribe en el archivo .txt
     file.write(text)
     file.close()
 
@@ -96,14 +110,91 @@ def geturl(filename):
     file.close()
 
 
+def detectar_idioma(text_to_detect):
+    text_to_detect = text_to_detect.lower()
+    tokens = nltk.tokenize.word_tokenize(text_to_detect)
+
+    # Creamos un dict donde almacenaremos la cuenta de las stopwords para cada idioma
+    lang_count = {}
+
+    # Variable para determinar el set de stopwords a utilizar según el lenguaje a detectar
+    current_lang_stop_words = stop_words_sp
+
+    for lang in languages:
+
+        if lang == "english":
+            current_lang_stop_words = stop_words_en
+
+        lang_count[lang] = 0  # Inicializa a 0 el contador para cada idioma
+
+        # Recorremos las palabras del texto a analizar
+        for word in tokens:
+            if word in current_lang_stop_words:  # Si la palabra se encuentra entre las stopwords, incrementa el contador
+                lang_count[lang] += 1
+
+    # Obtener y retornar el idioma con el número mayor de coincidencias
+    return max(lang_count, key=lang_count.get)
+
+
+'''
+Funciones para realizar limpieza a los textos
+'''
+
+
+# Esta funcion se encarga de llamar a las otras funciones encargadas de limpiar el texto en español,
+# al final me retorna el texto con todas las transformaciones que se le hicieron en la limpieza
+def limpieza_textos(text):
+
+    text = text.lower()
+    text = re.sub("\n+", " ", text)  # Eliminar saltos de linea
+    text = re.sub("\r+", " ", text)
+    text = re.sub("\t+", " ", text)
+    text = re.sub(r"[0-9]+", "", text)  # Eliminar cualquier numero del texto
+    text = EliminarSimbolos(text)
+    # text = re.sub("  +", " ", text)  # Eliminar espacios en blanco
+
+    # Hacer limpieza especificamente para textos en ingles
+    if detectar_idioma(text[0:80]) == "english":
+        text = limpieza_textos_en(text)
+
+    text = eliminar_stopwords(text)
+
+    return text
+
+
+def limpieza_textos_en(text):
+
+    text = expandir_contracciones(text)
+    text = lemmatize_words(text)
+
+    return text
+
+
 def EliminarSimbolos(text):
-    simbolosparaborrar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    simbolosparaborrar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~”—®»"
     for i in range(len(simbolosparaborrar)):
         text = text.replace(simbolosparaborrar[i], "")
     return text
 
-def eliminar_stopwords(texto):
-    return ' '.join([word for word in texto.split(' ') if word not in stop_words])
+
+def eliminar_stopwords(text):
+    return ' '.join([word for word in text.split(' ') if word not in stop_words])
+
+
+def expandir_contracciones(text):
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    return text
+
+
+def lemmatize_words(text):
+    return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
 
 
 geturl(enlaces)
--- PDF2IMG.py
+++ PDF2IMG.py
@@ -1,13 +1,14 @@
 # Import libraries
 from PIL import Image
-import pytesseract
-import requests
 from pdf2image import convert_from_path
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import os
 import errno
 import re
-from nltk.corpus import stopwords
-# import nltk    # Descomentar esto si es la primera vez que ejecuta el código
+import pytesseract
+import requests
+import nltk
 
 # Path of the PDF files
 path = "Transcripciones/"
@@ -15,13 +16,25 @@ path = "Transcripciones/"
 # Ruta en donde se encuentra el archivo .txt donde se almacena la lista de las url de descarga de los PDF
 enlaces = "PDFs.txt"
 
+# Definir los diferentes idiomas que aparecerán en las páginas:
+languages = ["spanish", "english"]
+
+'''
+Descomentar estas lineas de código si es la primera vez que ejecuta el código
+'''
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+
 # Crear una lista de las palabras "Stopwords" que se eliminarán del texto
-# nltk.download('stopwords')  # Descomentar esto si es la primera vez que ejecuta el código
 stop_words_sp = set(stopwords.words('spanish'))
 stop_words_en = set(stopwords.words('english'))
 
 # Concatenar las stopwords
 stop_words = stop_words_sp | stop_words_en
+
+# Definir un lematizador para el idioma inglés
+lemmatizer = WordNetLemmatizer()
 
 '''
 Part #1 : Downloading the PDF files
@@ -165,10 +178,11 @@ def extraertextodeimagenes(filelimit, cont, newpath):
 
 def escribir(filename, text):
     file = open(filename, "a", encoding="utf-8")  # Abrir en modo "a" Append
-    text = text.lower()
-    text = eliminar_stopwords(text)
-    text = EliminarSimbolos(text)
-    text = re.sub("\n+", " ", text)
+
+    # A continuación se hace el llamado a las funciones que realizan la limpieza del texto
+    text = limpieza_textos(text)
+
+    # Una vez hecha la limpieza, se escribe en el archivo .txt
     file.write(text)
 
 
@@ -211,8 +225,68 @@ def creardirectorio(url):
     return nuevodirectorio
 
 
+'''
+Part #5 - Cleanning text functions
+'''
+
+
+def detectar_idioma(text_to_detect):
+    text_to_detect = text_to_detect.lower()
+    tokens = nltk.tokenize.word_tokenize(text_to_detect)
+
+    # Creamos un dict donde almacenaremos la cuenta de las stopwords para cada idioma
+    lang_count = {}
+
+    # Variable para determinar el set de stopwords a utilizar según el lenguaje a detectar
+    current_lang_stop_words = stop_words_sp
+
+    for lang in languages:
+
+        if lang == "english":
+            current_lang_stop_words = stop_words_en
+
+        lang_count[lang] = 0  # Inicializa a 0 el contador para cada idioma
+
+        # Recorremos las palabras del texto a analizar
+        for word in tokens:
+            if word in current_lang_stop_words:  # Si la palabra se encuentra entre las stopwords, incrementa el contador
+                lang_count[lang] += 1
+
+    # Obtener y retornar el idioma con el número mayor de coincidencias
+    return max(lang_count, key=lang_count.get)
+
+
+# Esta funcion se encarga de llamar a las otras funciones encargadas de limpiar el texto en español,
+# al final me retorna el texto con todas las transformaciones que se le hicieron en la limpieza
+def limpieza_textos(text):
+
+    text = text.lower()
+    text = re.sub("\n+", " ", text)  # Eliminar saltos de linea
+    text = re.sub("\r+", " ", text)
+    text = re.sub("\t+", " ", text)
+    text = re.sub(r"[0-9]+", "", text)  # Eliminar cualquier numero del texto
+    text = EliminarSimbolos(text)
+    # text = re.sub("  +", " ", text)  # Eliminar espacios en blanco
+
+    # Hacer limpieza especificamente para textos en ingles
+    if detectar_idioma(text[0:80]) == "english":
+        text = limpieza_textos_en(text)
+
+    text = eliminar_stopwords(text)
+
+    return text
+
+
+def limpieza_textos_en(text):
+
+    text = expandir_contracciones(text)
+    text = lemmatize_words(text)
+
+    return text
+
+
 def EliminarSimbolos(text):
-    simbolosparaborrar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    simbolosparaborrar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~”—®»"
     for i in range(len(simbolosparaborrar)):
         text = text.replace(simbolosparaborrar[i], "")
     return text
@@ -220,6 +294,22 @@ def EliminarSimbolos(text):
 
 def eliminar_stopwords(texto):
     return ' '.join([word for word in texto.split(' ') if word not in stop_words])
+
+
+def expandir_contracciones(text):
+    text = re.sub(r"n\'t", " not", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'s", " is", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'t", " not", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'m", " am", text)
+    return text
+
+
+def lemmatize_words(text):
+    return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
 
 
 geturl(enlaces)
