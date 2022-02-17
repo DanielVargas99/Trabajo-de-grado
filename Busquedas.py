78f3fb2dcb6f987421684c426982aa2bbbeada04
DaniiVCGamer <daniel.vargas@correounivalle.edu.co>
2022-02-16 21:28:09 -0500

    Cuarto commit

--- Busquedas.py
+++ Busquedas.py
@@ -94,6 +94,7 @@ def principal():
             with open(resumen, encoding='utf-8') as a: # Se abre el archivo que contiene los resumenes de las becas
                 reader = csv.reader(a, delimiter=';')
                 for row in reader:
+                    print(row[0], lista_ordenada[i][0])
                     if row[0] == lista_ordenada[i][0]:
                         beca_seleccionada = {
                             "Documento": row[0],
@@ -106,7 +107,6 @@ def principal():
                         }
             lista_aux.append(beca_seleccionada)
 
-        print("PRUEBAAAAAAAAAAAAAA: ",lista_aux)
         return lista_aux
 
     def limpieza_busqueda(text_en, text_sp):
@@ -238,7 +238,6 @@ def principal():
     resultados_busqueda = buscar_palabra_en_lista_csv(csvs, busqueda)  # Obtener los documentos que más se ajusten a la busqueda
     sorted_list = sorted(resultados_busqueda, key=lambda aux: (aux[1], aux[2]), reverse=True)  # Ordenar la lista de mayor a menor
 
-    print(sorted_list)
     return jsonify({"BECAS": imprimir_resultados(sorted_list)})
 
 if __name__ == '__main__':
